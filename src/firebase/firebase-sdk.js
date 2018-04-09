98ca6b9f1c2cff143b7b5d7ff404b7bde16a05bd
Steinroe <philipp@steinroetter.com>
2018-04-09 18:49:55 +0200

    #12 delete old stuff

--- src/firebase/firebase-sdk.js
+++
@@ -1,9 +0,0 @@
-
-var admin = require('firebase-admin')
-
-var serviceAccount = require('path/to/serviceAccountKey.json')
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://lunchapp-e3a2d.firebaseio.com'
-})
